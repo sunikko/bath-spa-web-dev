300ba96f1742747a32b3d488da2022d749396a3e
sunikko <sunikko77@gmail.com>
2022-10-24 13:23:54 +0100

    amend toggle function

--- Session10/function.js
+++ Session10/function.js
@@ -1,7 +1,14 @@
-function turnLightOn() {
-    document.getElementById("lightImg").src = "./img/pic_bulbon.gif"
-}
+function toggleLight() {
+    let bulbObj = document.getElementById("img-bulb");
+    let bOnOff = bulbObj.getAttribute('data-onoff');
 
-function turnLightOff() {
-    document.getElementById("lightImg").src = "./img/pic_bulboff.gif"
+    console.log(bOnOff);
+
+    if(bOnOff == "off"){
+        bulbObj.src = "./img/pic_bulbon.gif"
+        bulbObj.setAttribute('data-onoff', "on");
+    }else{
+        bulbObj.src = "./img/pic_bulboff.gif"
+        bulbObj.setAttribute('data-onoff', "off");
+    }
 }
