b5377f59817cd68923991f06232b4d5b04be0fb5
sunikko <sunikko77@gmail.com>
2022-10-24 18:42:26 +0100

    create BMI calculator html/js

--- Session10/function.js
+++ Session10/function.js
@@ -12,3 +12,22 @@ function toggleLight() {
         bulbObj.setAttribute('data-onoff', "off");
     }
 }
+
+function calBMI(){
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+
+    let squreHeight = (height/100) * (height/100);
+    let bmi = weight/squreHeight
+    let classification = '';
+
+    if(bmi <18.50)
+        classification = 'Underweight';
+    else if(bmi >18.50 && bmi< 24.99)
+        classification = 'Normal range';
+    else
+        classification = 'Overweight';
+
+    document.getElementById("result").innerHTML =  `Your BMI: ${bmi.toFixed(2)}<br> Classification: ${classification}`;
+    
+}
