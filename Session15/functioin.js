c93b607750208c2c9a0fd708e3e71a38527913c7
sunikko <sunikko77@gmail.com>
2022-11-05 13:02:59 +0000

    Update functioin.js

--- Session15/functioin.js
+++ Session15/functioin.js
@@ -1,42 +1,51 @@
-// const plus = document.getElementById("plus");
 const input = document.getElementById("input");
 const inputplus = document.getElementById("input-plus");
 let input_A = 0.0;
 let input_B = 0.0;
+let input_operator = '';
 
-// plus.addEventListener("click", () => {
-    
-// });
-function inputClear(btnText){
+const inputClear = (btnText) => {
     input.value = '';
     inputplus.value = '';
 }
-function inputPlus(btnText){
+const inputPlus = (btnText) => {
     input_A = parseFloat(input.value);
     console.log(input_A);
-    inputplus.value += input_A;
-    inputplus.value += btnText;
+    inputplus.value = input_A + btnText;
     input.value = '';
+    input_operator = btnText;
 }
-
-function inputMinus(btnText){
+const inputMinus = (btnText) => {
+    input_A = parseFloat(input.value);
+    console.log(input_A);
+    inputplus.value = input_A + btnText;
+    input.value = '';
+    input_operator = btnText;
+}
+const inputMultiply = (btnText) => {
 
 }
-
-function inputMultiply(btnText){
-
-}
-
-function inputDivide(btnText){
+const inputDivide = (btnText) => {
 
 }
-
-function inputResult(btnText){
+const inputResult = (btnText) => {
     input_B = parseFloat(input.value);
-    inputplus.value += input_B;
-    inputplus.value += btnText;
+    inputplus.value += (input_B + btnText);
     console.log(input_A + input_B);
-    input.value = input_A + input_B;
+    switch(input_operator){
+        case '+':
+            input.value = input_A + input_B;
+            break;
+        case '-':
+            input.value = (input_A - input_B).toPrecision(6);; //Math.round
+            break;
+        case '*':
+            input.value = input_A * input_B;
+            break;
+        case '/':
+        input.value = input_A / input_B;
+        break;
+    }
 }
 
 const calculationMap = {
@@ -47,10 +56,14 @@ const calculationMap = {
         inputPlus(btnText);
     },
     minus(btnText){
-        inputDivide(btnText);
+        alert("-");
+        inputMinus(btnText);
     },
     multiply(btnText){
         inputMultiply(btnText);
+    },
+    divide(btnText){
+        inputDivide(btnText);
     },
     result(btnText){
         inputResult(btnText);
@@ -66,12 +79,49 @@ const operatorMap = {
     "=" : "result"
 }
 
-function executeInput(elem){
+const executeInput = (elem) => {
     var btnText = elem.textContent || elem.innerText;
     console.log(btnText);    
     if(isNaN(btnText) && '.' != btnText){
+        //alert(calculationMap['minus']);
         calculationMap[operatorMap[btnText]](btnText);
     }else{
         input.value += btnText;
     }
 }
+
+/*
+//second try
+function addInput(elem){
+    var btnText = elem.textContent || elem.innerText;
+    console.log(btnText);    
+    switch(btnText){
+        case 'C':
+            input.value = '';
+            inputplus.value = '';
+            break;
+        case '+':
+            input_A = parseFloat(input.value);
+            console.log(input_A);
+            inputplus.value += input_A;
+            inputplus.value += btnText;
+            input.value = '';
+            break;
+        case '=':
+            input_B = parseFloat(input.value);
+            inputplus.value += input_B;
+            inputplus.value += btnText;
+            console.log(input_A + input_B);
+            input.value = input_A + input_B;
+            break;
+        default:
+            input.value += btnText;
+    }
+}
+
+//first try
+const plus = document.getElementById("plus");
+plus.addEventListener("click", () => {
+    
+ });
+*/
