e8abba7fba4463b5c292db9b36943c62400e1b73
sunikko <sunikko77@gmail.com>
2022-11-05 02:56:23 +0000

    refactor
    use mapping -  function name

--- Session15/functioin.js
+++ Session15/functioin.js
@@ -7,31 +7,71 @@ let input_B = 0.0;
 // plus.addEventListener("click", () => {
     
 // });
+function inputClear(btnText){
+    input.value = '';
+    inputplus.value = '';
+}
+function inputPlus(btnText){
+    input_A = parseFloat(input.value);
+    console.log(input_A);
+    inputplus.value += input_A;
+    inputplus.value += btnText;
+    input.value = '';
+}
 
+function inputMinus(btnText){
 
-function addInput(elem){
+}
+
+function inputMultiply(btnText){
+
+}
+
+function inputDivide(btnText){
+
+}
+
+function inputResult(btnText){
+    input_B = parseFloat(input.value);
+    inputplus.value += input_B;
+    inputplus.value += btnText;
+    console.log(input_A + input_B);
+    input.value = input_A + input_B;
+}
+
+const calculationMap = {
+    clear(btnText){
+        inputClear(btnText);
+    },
+    plus(btnText){
+        inputPlus(btnText);
+    },
+    minus(btnText){
+        inputDivide(btnText);
+    },
+    multiply(btnText){
+        inputMultiply(btnText);
+    },
+    result(btnText){
+        inputResult(btnText);
+    }
+}
+
+const operatorMap = {
+    'C' : "clear",
+    "+" : "plus",
+    "-" : "minus",
+    "*" : "multiply",
+    "/" : "divide",
+    "=" : "result"
+}
+
+function executeInput(elem){
     var btnText = elem.textContent || elem.innerText;
     console.log(btnText);    
-    switch(btnText){
-        case 'C':
-            input.value = '';
-            inputplus.value = '';
-            break;
-        case '+':
-            input_A = parseFloat(input.value);
-            console.log(input_A);
-            inputplus.value += input_A;
-            inputplus.value += btnText;
-            input.value = '';
-            break;
-        case '=':
-            input_B = parseFloat(input.value);
-            inputplus.value += input_B;
-            inputplus.value += btnText;
-            console.log(input_A + input_B);
-            input.value = input_A + input_B;
-            break;
-        default:
-            input.value += btnText;
+    if(isNaN(btnText) && '.' != btnText){
+        calculationMap[operatorMap[btnText]](btnText);
+    }else{
+        input.value += btnText;
     }
 }
